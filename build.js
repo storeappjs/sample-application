0c07a070db59ffb619f119a842c98e0639a70511
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-06-07 22:47:45 +0800

    upload sample project

---
+++ build.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const { join } = require("path");
+
+console.log("Building..");
+
+fs.writeFileSync(
+  join(__dirname, "build.js"),
+  `console.log("Hello, World!"); /*print hello world*/`
+);
